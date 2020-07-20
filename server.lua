366bba6e6216f8334ce95913910db1c8fd508f2e
Jared <TheWolfBadger@gmail.com>
2020-07-20 18:13:38 -0400

    Add files via upload

--- server.lua
+++ server.lua
@@ -5,13 +5,19 @@
 --- Code ---
 
 roleList = {
-{1, "group.tc"}, --[[ Trusted-Civ --- ]] 
-{1, "group.faa"}, --[[ FAA --- ]]
-{1, "group.donator"}, --[[ Donator --- ]]
-{1, "group.trialModerator"}, --[[ T-Mod --- ]] 
+{1, "group.tc"}, -- Trusted Civ
+{1, "group.ems"}, -- EMS
+{1, "group.discord"}, -- DISCORD MEMBER
+{1, "group.co"}, -- Community Cop
+{1, "group.nitro"}, -- Nitro Booster
+{1, "group.faaheli"}, --[[ FAA heli --- ]]
+{1, "group.faacommercial"}, --[[ FAA planes--- ]]
+{1, "group.donatormenu2"}, --[[ Donator Menu 2--- ]]
+{1, "group.donatormenu1"}, --[[ Donator Menu 1--- ]]
+{1, "group.trialmoderator"}, --[[ T-Mod --- ]] 
 {1, "group.moderator"}, --[[ Moderator --- ]]
 {1, "group.admin"}, --[[ Admin --- ]]
-{1, "group.admin"}, --[[ Management --- ]]
+{1, "group.management"}, --[[ Management --- ]]
 {1, "group.owner"}, --[[ Owner --- ]]
 }
 local function has_value (tab, val)
@@ -51,38 +57,82 @@ function stringsplit(inputstr, sep)
 end
 
 hasPermsAlready = {}
+discordDetector = {}
 
-AddEventHandler('playerConnecting', function()
-	local src = source
-	local hex = string.sub(tostring(PlayerIdentifier("steam", src)), 7)
-	permAdd = "add_principal identifier.steam:" .. hex .. " "
-	for k, v in ipairs(GetPlayerIdentifiers(src)) do
-			if string.sub(v, 1, string.len("discord:")) == "discord:" then
-				identifierDiscord = v
-			end
-	end
-	if identifierDiscord then
-		if not has_value(hasPermsAlready, GetPlayerName(src)) then
-			local roleIDs = exports.discord_perms:GetRoles(src)
-			if not (roleIDs == false) then
-				for i = 1, #roleList do
-					for j = 1, #roleIDs do
-						if (tostring(roleList[i][1]) == tostring(roleIDs[j])) then
-							print("Added " .. GetPlayerName(src) .. " to role group " .. roleList[i][2] .. " with discordRole ID: " .. roleIDs[j])
-							ExecuteCommand(permAdd .. roleList[i][2])
+AddEventHandler('playerConnecting', function(name, setKickReason, deferrals)
+	deferrals.defer();
+	local src = source; 
+	if not has_value(hasPermsAlready, PlayerIdentifier('discord', src)) then
+		local dis = string.sub(tostring(PlayerIdentifier("discord", src)), 7)
+		permAdd = "add_principal identifier.discord:" .. dis .. " "
+		for k, v in ipairs(GetPlayerIdentifiers(src)) do
+				if string.sub(v, 1, string.len("discord:")) == "discord:" then
+					identifierDiscord = v
+				end
+		end
+		if identifierDiscord then
+			if not has_value(hasPermsAlready, PlayerIdentifier('steam', src)) then
+				local roleIDs = exports.discord_perms:GetRoles(src)
+				if not (roleIDs == false) then
+					for i = 1, #roleList do
+						for j = 1, #roleIDs do
+							if (tostring(roleList[i][1]) == tostring(roleIDs[j])) then
+								print("[DiscordAcePerms] Added " .. GetPlayerName(src) .. " to role group " .. roleList[i][2] .. " with discordRole ID: " .. roleIDs[j])
+								ExecuteCommand(permAdd .. roleList[i][2])
+							end
 						end
 					end
+					table.insert(hasPermsAlready, PlayerIdentifier('discord', src))
+				else
+					print("[DiscordAcePerms] " .. GetPlayerName(src) .. " has not gotten their permissions cause roleIDs == false")
 				end
-				table.insert(hasPermsAlready, GetPlayerName(src))
-			else
-				print(GetPlayerName(src) .. " has not gotten their permissions cause roleIDs == false")
+			end
+		else 
+			if not has_value(discordDetector, PlayerIdentifier('discord', src)) then 
+				-- Kick with we couldn't find their discord, try to restart it whilst fivem is closed 
+				deferrals.done('[DiscordAcePerms] DISCORD NOT FOUND... Try restarting Discord application whilst FiveM is closed! ' ..
+					'This notice will not be displayed to you upon next connect.')
+				table.insert(discordDetector, PlayerIdentifier('discord', src));
+				print('[DiscordAcePerms] Discord was not found for player ' .. GetPlayerName(src) .. "...")
+				CancelEvent();
+				return;
 			end
 		end
 	end
+	deferrals.done();
 end)
 
+
+-- @Deprecated 
 RegisterServerEvent("DiscordAcePerms:GivePerms")
-AddEventHandler("DiscordAcePerms:GivePerms", function(_source)
-	-- Deprecated
+AddEventHandler("DiscordAcePerms:GivePerms", function()
+	local src = source
+	if not has_value(hasPermsAlready, PlayerIdentifier('discord', src)) then
+		local dis = string.sub(tostring(PlayerIdentifier("discord", src)), 7)
+		permAdd = "add_principal identifier.discord:" .. dis .. " "
+		for k, v in ipairs(GetPlayerIdentifiers(src)) do
+				if string.sub(v, 1, string.len("discord:")) == "discord:" then
+					identifierDiscord = v
+				end
+		end
+		if identifierDiscord then
+			if not has_value(hasPermsAlready, PlayerIdentifier('discord', src)) then
+				local roleIDs = exports.discord_perms:GetRoles(src)
+				if not (roleIDs == false) then
+					for i = 1, #roleList do
+						for j = 1, #roleIDs do
+							if (tostring(roleList[i][1]) == tostring(roleIDs[j])) then
+								print("[DiscordAcePerms] Added " .. GetPlayerName(src) .. " to role group " .. roleList[i][2] .. " with discordRole ID: " .. roleIDs[j])
+								ExecuteCommand(permAdd .. roleList[i][2])
+							end
+						end
+					end
+					table.insert(hasPermsAlready, PlayerIdentifier('discord', src))
+				else
+					print("[DiscordAcePerms] " .. GetPlayerName(src) .. " has not gotten their permissions cause roleIDs == false")
+				end
+			end
+		end
+	end
 end)
 			
