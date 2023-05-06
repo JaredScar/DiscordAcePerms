d17985c2e0b657b44c75fafb71c2d1bf7a161299
Jared <TheWolfBadger@gmail.com>
2023-05-06 17:05:22 -0400

    Added update script for version checking

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,5 +5,6 @@
 fx_version 'cerulean'
 game 'gta5'
 
+server_script "versionChecker.lua"
 server_script "config.lua"
 server_script "server.lua"
---
+++ versionChecker.lua
@@ -0,0 +1,20 @@
+Citizen.CreateThread(function()
+    updatePath = "/JaredScar/DiscordAcePerms" -- your git user/repo path
+    resourceName = "DiscordAcePerms ("..GetCurrentResourceName()..")" -- the resource name
+    
+    function checkVersion(err,responseText, headers)
+        curVersion = LoadResourceFile(GetCurrentResourceName(), "version") -- make sure the "version" file actually exists in your resource root!
+    
+        if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
+            print("\n###############################")
+            print("\n"..resourceName.." is outdated, should be:\n"..responseText.."is:\n"..curVersion.."\nplease update it from https://github.com"..updatePath.."")
+            print("\n###############################")
+        elseif tonumber(curVersion) > tonumber(responseText) then
+            print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online I advise you to update...")
+        else
+            print("\n"..resourceName.." is up to date!")
+        end
+    end
+    
+    PerformHttpRequest("https://raw.githubusercontent.com/JaredScar/DiscordAcePerms/master/version.txt", checkVersion, "GET")
+end)
